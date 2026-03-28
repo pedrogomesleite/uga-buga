72e34a5c7d9c1f8b7c16d112a8cea9e7ea2d4366
Joao <joaovitormadeiro@gmail.com>
2026-03-28 04:46:52 -0300

    movimentação aprimorada

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -1,27 +1,24 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-@export var forca_impacto: float = 50.0  
-@export var friccao_empurrao: float = 20.0 
+const SPEED = 5.0
+@export var forca_impacto: float = .0625
+var impulso_max: float = 200
 
 var empurrao_velocity: Vector2 = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
-	empurrao_velocity = empurrao_velocity.lerp(Vector2.ZERO, friccao_empurrao * delta)
-	
-	velocity += empurrao_velocity
+	else:
+		#atrito
+		velocity.x = lerp(velocity.x, 0.0, 5.0 * delta)
+		
 	move_and_slide()
-	
-	velocity.x = 0 
 
-func _on_pau_objeto_tocado(corpo: Variant, direcao: Variant) -> void:
-	var posicao_relativa = corpo.global_position - global_position
-	
-	var vel_mouse = remap(Input.get_last_mouse_velocity().length(), 0, 2000, 0, 2)
-	var forca_final = forca_impacto
-	print(-posicao_relativa.normalized(), -direcao, position)
-	var vetor_impulso = posicao_relativa.normalized() * forca_final
-	empurrao_velocity = vetor_impulso.limit_length(2000.0)
+func _on_pau_objeto_tocado(vel_martelo: Vector2, _normal: Vector2) -> void:
+	var vetor_impulso = -vel_martelo * forca_impacto
+	vetor_impulso = vetor_impulso.limit_length(impulso_max)
+	var velocidade_max: float= SPEED * Input.get_last_mouse_velocity().length()
+	velocity += vetor_impulso
+	velocity = velocity.limit_length(velocidade_max)
+	print(velocity)
--- scripts/pau.gd
+++ scripts/pau.gd
@@ -1,29 +1,29 @@
 extends CharacterBody2D
 
 
-signal objeto_tocado(corpo, direcao)
+signal objeto_tocado(vel_martelo, normal)
 
-@export var velocidade_rotacao: float = 2
+@export var velocidade_rotacao: float = 20.0
 @export var raio_maximo: float = 100.0
-@export var velocidade_movimento: float = 4.0
+@export var velocidade_movimento: float = 15.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	var vel_mouse = Input.get_last_mouse_velocity()
 	var mouse_relativo = get_parent().get_local_mouse_position()
 	# ir em direcao ao mouse
+	
 	var alvo_limitado = mouse_relativo.limit_length(raio_maximo)
-	position = position.lerp(alvo_limitado, velocidade_movimento * delta)
+	var direcao = alvo_limitado - position
+	var velocidade_adicional = direcao * velocidade_movimento
 	
 	# girar em direcao ao mouse
 	var angulo_alvo = mouse_relativo.angle()
 	rotation = lerp_angle(rotation, angulo_alvo + PI/2, velocidade_rotacao * delta)
 	
-	checar_colisoes()
+	checar_colisoes(velocidade_adicional, delta)
 
-func checar_colisoes():
-	var colisao = move_and_collide(Vector2.ZERO)
+func checar_colisoes(velocidade: Vector2, delta: float):
+	var colisao = move_and_collide(velocidade * delta)
 	if colisao:
-		var corpo = colisao.get_collider()
-		var direcao = (corpo.global_position - global_position).normalized()
-		objeto_tocado.emit(corpo, direcao)
+		var normal = colisao.get_normal()
+		objeto_tocado.emit(velocidade, normal)
