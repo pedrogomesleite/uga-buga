3a37ba60be0d4555c4b371df6cda7f1a34295681
pedrogomesleite <pedro.gomes.leite@gmail.com>
2026-03-30 00:21:55 -0300

    portal para maloca

--- scripts/saida_caverna.gd
+++ scripts/saida_caverna.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
+@export var next_scene = "res://scenes/cenarios/Floresta.tscn"
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
-		Controller.trocar_cena("res://scenes/cenarios/Floresta.tscn")
+		Controller.trocar_cena(next_scene)
