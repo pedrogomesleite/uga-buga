9c295288384d4083aa0e10e78d4035f6dbac0fb9
pedrogomesleite <pedro.gomes.leite@gmail.com>
2026-03-29 21:16:30 -0300

    homem sombra apanhavel

---
+++ scripts/homem_sombra_empurravel.gd
@@ -0,0 +1,29 @@
+extends RigidBody2D
+
+@export var timer: float = 5.0
+var colide = true
+
+func _ready() -> void:
+	freeze = true
+	collision_layer = 0  
+	collision_mask = 0  
+
+func _process(delta: float) -> void:
+	var colliding_bodies = get_colliding_bodies()
+	if colliding_bodies.any(func(e): return e is CharacterBody2D):
+		if colide: # Verifica se já não começou o processo de sumir
+			colide = false
+			await get_tree().create_timer(timer).timeout
+			print("chegeui oloco")
+			queue_free()
+
+func _on_area_2d_body_entered(_body: Node2D) -> void:
+	if _body is CharacterBody2D:
+		set_collision_layer_value(1, true)
+		set_collision_layer_value(2, true)
+		
+		set_collision_mask_value(1, true)
+		set_collision_mask_value(2, true)
+		
+		set_deferred("freeze", false)
+		set_deferred("sleeping", false)
